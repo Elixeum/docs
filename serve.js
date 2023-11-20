91b8ce9697b8463d17900cf6315d11c4e66f9368
Martin Pernica <martinpernica@outlook.com>
2023-11-20 22:23:17 +0100

    Update serve.js

--- serve.js
+++ serve.js
@@ -2,55 +2,55 @@
 // By-design this web server does not use any external packages so it can be run as-is without installing any dependencies.
 // Yet it is very dumb and insecure, so use it wisely!
 
-const http = require('node:http');
-const fs = require('node:fs');
-const path = require('node:path');
-const crypto = require('node:crypto');
+const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
+const crypto = require("node:crypto");
 
 const args = process.argv.slice(2);
 const nodeMajorVersion = process.version.match(/^v(\d+)/)[1]; // Omit minor, patch version from version string
 
 // At least `crypto` package requires Node 15+
 if (nodeMajorVersion < 15) {
-    console.error('This server requires Node.js v15 or higher. Please upgrade your Node.js installation.');
-    process.exit(1);
+  console.error("This server requires Node.js v15 or higher. Please upgrade your Node.js installation.");
+  process.exit(1);
 }
 
-let httpProtocol = 'http';
+let httpProtocol = "http";
 let httpPort = 8080;
-let httpHostname = 'localhost';
+let httpHostname = "localhost";
 let isVerbose = false;
-let serveRoot = __dirname + path.sep + 'docs';
+let serveRoot = __dirname + path.sep + "docs";
 
 // Map for file aliases, especially to support empty request to map it to index.html
 const aliases = {
-    '/': '/index.html'
+  "/": "/index.html",
 };
 
-// List of possible insercure paths, which are not allowed to be accessed
-const insecurePaths = ['..'];
+// List of possible insecure paths, which are not allowed to be accessed
+const insecurePaths = [".."];
 
-// Trival argument parsing (not foolproof):
+// Trivial argument parsing (not foolproof):
 // --port <port>
 // --verbose
 // --hostname <hostname>
 for (let i = 0; i < args.length; i++) {
-    switch (args[i]) {
-        case '--port':
-            httpPort = parseInt(args[i + 1]);
-            break;
-        case '--verbose':
-            isVerbose = true;
-            break;
-        case '--hostname':
-            httpHostname = args[i + 1];
-            break;
-    }
+  switch (args[i]) {
+    case "--port":
+      httpPort = parseInt(args[i + 1]);
+      break;
+    case "--verbose":
+      isVerbose = true;
+      break;
+    case "--hostname":
+      httpHostname = args[i + 1];
+      break;
+  }
 }
 
 // Startup info
 const title = "Elixeum Simple Web Server";
-console.log(title + '\n' + '='.repeat(title.length));
+console.log(title + "\n" + "=".repeat(title.length));
 
 console.log(`Starting server on port ${httpPort}`);
 console.log(`Serving from ${serveRoot}`);
@@ -58,9 +58,10 @@ console.log(`Verbose mode: ${isVerbose}`);
 console.log(`Open web browser at ${httpProtocol}://${httpHostname}:${httpPort}`);
 
 // Very basic - **insecure** - http server to serve static files
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     let file = serveRoot + req.url;
-    let requestId = crypto.randomBytes(3 * 4).toString('base64'); // ID for logging to distinquish requests from each other
+    let requestId = crypto.randomBytes(3 * 4).toString("base64"); // ID for logging to distinquish requests from each other
     let timeNow = new Date();
 
     let timeMarker = `[${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}]`;
@@ -68,40 +69,41 @@ http.createServer((req, res) => {
     let outputMarker = `${timeMarker} ${requestMarker}`;
 
     if (isVerbose) {
-        console.log(`${outputMarker} HTTP ${req.method} ${req.url}`);
+      console.log(`${outputMarker} HTTP ${req.method} ${req.url}`);
     }
 
     if (aliases[req.url]) {
-        if (isVerbose) {
-            console.log(`${outputMarker} Request matches aliased path ${req.url} => ${aliases[req.url]}`);
-        }
+      if (isVerbose) {
+        console.log(`${outputMarker} Request matches aliased path ${req.url} => ${aliases[req.url]}`);
+      }
 
-        file = serveRoot + aliases[req.url];
+      file = serveRoot + aliases[req.url];
     }
 
     for (const insecurePath of insecurePaths) {
-        if (file.indexOf(insecurePath) !== -1) {
-            console.warn(`${outputMarker} Request matches insecure path!`);
+      if (file.indexOf(insecurePath) !== -1) {
+        console.warn(`${outputMarker} Request matches insecure path!`);
 
-            res.writeHead(403, { 'Content-Type': 'text/plain' });
-            res.end('403 Forbidden');
-            return;
-        }
+        res.writeHead(403, { "Content-Type": "text/plain" });
+        res.end("403 Forbidden");
+        return;
+      }
     }
 
     fs.readFile(file, (err, data) => {
-        if (isVerbose) {
-            console.log(`${outputMarker} IO read to ${file}`);
-        }
+      if (isVerbose) {
+        console.log(`${outputMarker} IO read to ${file}`);
+      }
 
-        if (err) {
-            res.writeHead(404);
-            res.end('404 Not Found');
+      if (err) {
+        res.writeHead(404);
+        res.end("404 Not Found");
 
-            return;
-        }
+        return;
+      }
 
-        res.writeHead(200);
-        res.end(data);
+      res.writeHead(200);
+      res.end(data);
     });
-}).listen(httpPort);
+  })
+  .listen(httpPort);
